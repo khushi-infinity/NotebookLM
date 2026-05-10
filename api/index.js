aca094cbe904bfef4f6be0b31b3e3aabebca95f6
Khushi Sarawagi <khushikhush006@gmail.com>
2026-05-10 12:21:35 +0530

    Fix: Move dotenv to local index.js, remove from app.js for serverless

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
 import app from "../backend/src/app.js";
 
+console.log("[Vercel API] App loaded");
+
 export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import app from "./app.js";
 import express from "express";
 import path from "path";
